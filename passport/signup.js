166cc85e83c2644dd537437c912d76b8853a0c78
sekhar karri <sekhar.karri.ext@lvs.co.uk>
2017-06-13 17:10:17 +0100

    Hashing passsword

--- passport/signup.js
+++ passport/signup.js
@@ -1,13 +1,18 @@
-var LocalStrategy = require('passport-local').Strategy;
-var User = require('../models/user');
-var bCrypt = require('bcrypt-nodejs');
-module.exports = function(passport, dbs) {
+  var LocalStrategy = require('passport-local').Strategy;
+  var User = require('../models/user');
+  var bCrypt = require('bcrypt-nodejs');
+  module.exports = function(passport, dbs) {
 
-  passport.use('signup', new LocalStrategy({
-    passReqToCallback: true // allows us to pass back the entire request to the callback
-  }, function(req, username, password, done) {    
-      dbs.production.collection('users').insertOne(req.body, function(err, res){
-        if(err) console.log('err', err);
+    passport.use('signup', new LocalStrategy({
+      passReqToCallback: true // allows us to pass back the entire request to the callback
+    }, function(req, username, password, done) {
+      req.body.password = createHash(password);
+      dbs.production.collection('users').insertOne(req.body, function(err, res) {
+        if (err) console.log('err', err);
       });
-  }));
-}
+    }));
+
+    var createHash = function(password) {
+        return bCrypt.hashSync(password,bCrypt.genSaltSync(10), null);
+    }
+  }
