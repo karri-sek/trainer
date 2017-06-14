e6244edad00611b6328dee7170f9b936c46c68e8
sekhar karri <sekhar.karri.ext@lvs.co.uk>
2017-06-14 14:57:28 +0100

    Wrapping the get user function into a promise and make sure the successive function call from a then clause

--- passport/signup.js
+++ passport/signup.js
@@ -1,8 +1,8 @@
   var LocalStrategy = require('passport-local').Strategy;
   var User = require('../models/user');
   var bCrypt = require('bcrypt-nodejs');
+  var async = require('async');
   module.exports = function(passport, dbs, logger) {
-
     passport.use('signup', new LocalStrategy({
       passReqToCallback: true // allows us to pass back the entire request to the callback
     }, function(req, username, password, done) {
@@ -13,27 +13,9 @@
         if (doc) {
           logger.log('error', 'user already exists with user name %s', username)
         } else {
-          dbs.production.collection('users').find({}).sort({
-            "userid": -1
-          }).limit(1).toArray(function(err, users) {
-            var user = new User(req.body);
-            user.password = createHash(password);
-            if (users.length != 0) {
-              user.userid = users[0].userid + 1;
-            } else {
-              user.userid = 1;
-            }
-            dbs.production.collection('users').insertOne(user,
-              function(err, result) {
-                if (err) {
-                  logger.log('error', 'Unable to create user with username: %s', username)
-                  res.error(err);
-                } else {
-                  logger.log('info', 'user %s created successfully.', username)
-                  done(null, result.ops[0]);
-                }
-              });
-          });
+          getUser(req, password).
+          then(user => insertUserRecord(user)).
+          catch(err => console.log('err', err));
         }
       });
     }));
@@ -41,4 +23,33 @@
     var createHash = function(password) {
       return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
     }
+
+    var getUser = (req, password) =>
+      new Promise((resolve, reject) => {
+        dbs.production.collection('users').find({}).sort({
+          "userid": -1
+        }).limit(1).toArray((err, users) => {
+          if (err) reject(err)
+          else {
+            var user = new User(req.body);
+            user.password = createHash(password);
+            if (users.length != 0) {
+              user.userid = users[0].userid + 1;
+            } else {
+              user.userid = 1;
+            }
+            resolve(user);
+          }
+        });
+      });
+
+    var insertUserRecord = (user) =>
+      dbs.production.collection('users').insertOne(user, function(err, result) {
+        if (err) {
+          logger.log('error', 'Unable to create user with username: %s', user.username)
+          res.error(err);
+        } else {
+          logger.log('info', 'user %s created successfully.', user.username)
+        }
+      });
   }
